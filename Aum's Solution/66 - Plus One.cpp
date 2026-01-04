0ead63b45830055f20b90de04feade06631a1d46
aumdave1979 <aumdave1979@gmail.com>
2026-01-04 18:57:03 +0530

    66

--- Aum's Solution/66 - Plus One.cpp
+++ Aum's Solution/66 - Plus One.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i=digits.size()-1;i>=0;i--){
-            if(digits[i]<9){
+            if(digits[i]<9){  
                 digits[i]++;
                 return digits;
             }

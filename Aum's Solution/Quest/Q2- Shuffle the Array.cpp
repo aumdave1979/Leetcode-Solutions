613e39343a8a816273609f6f4306a2e669c37986
Aum Dave <aumdave1979@gmail.com>
2026-01-21 16:21:42 +0530

    quest q2

---
+++ Aum's Solution/Quest/Q2- Shuffle the Array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+     int i=0;
+     int j =n;
+     vector<int> ans;
+     while(i<n && j<nums.size()){
+        ans.push_back(nums[i]);
+        ans.push_back(nums[j]);
+        i++;
+        j++;
+     }   
+    return ans;
+    }
+};

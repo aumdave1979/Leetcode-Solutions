1f10821829c2a749bc8a97127174ad16128793c0
Aum Dave <aumdave1979@gmail.com>
2026-03-18 06:59:15 +0530

    31

---
+++ Aum's Solution/31 - Next Permuation.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Sol1 A built in function not recommended for learning;
+
+/*class Solution {
+ public:
+     void nextPermutation(vector<int>& nums) {
+         // a built in function not recommended to use >❌
+         next_permutation(nums.begin(),nums.end());
+     }
+ };*/

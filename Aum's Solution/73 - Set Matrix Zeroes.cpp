4f8c4612aa3af769484aa0249b6ded9f12f1c78b
Aum Dave <aumdave1979@gmail.com>
2026-03-22 16:30:31 +0530

    updated

--- Aum's Solution/73 - Set Matrix Zeroes.cpp
+++ Aum's Solution/73 - Set Matrix Zeroes.cpp
@@ -1,26 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-    int m = matrix.size();
-    int n = matrix[0].size();
-    vector <vector<int>> positions;
-    for (int i=0; i<m;i++)
-    {
-        for (int j=0;j<n; j++)
-        {
-            if (matrix[i][j] == 0)
-            {
-                positions.push_back({i, j});
+
+
+// brute force solution 
+/*
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector <vector<int>> positions;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    positions.push_back({i, j});
+                }
+            }
+        }
+        for (auto pos : positions) {
+            int row = pos[0];
+            int col = pos[1];
+
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
             }
         }
     }
-    for(int i=0;i<positions.size();i++){
-        for(int j=0;j<positions[i].size();j++){
-            cout << positions[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+};
+*/

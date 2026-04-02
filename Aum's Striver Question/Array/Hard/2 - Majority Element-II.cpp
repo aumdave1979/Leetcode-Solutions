db34d271d76f02af63d3d3a3c3b63cfe973754ba
aumdave1979 <aumdave1979@gmail.com>
2026-04-02 16:29:30 +0530

    optimied Majority element

--- Aum's Striver Question/Array/Hard/2 - Majority Element-II.cpp
+++ Aum's Striver Question/Array/Hard/2 - Majority Element-II.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+// optimal NlogN time complexity and O(1) space complexity
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int times = n/3;
+        vector<int> ans;
+        sort(nums.begin(),nums.end());
+        int count =1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                count++;
+            }
+            else{
+                if(count>times){
+                    ans.push_back(nums[i-1]);
+                }
+                count=1;
+            }
+        }
+        if(count>times){
+            ans.push_back(nums[n-1]);
+        }
+    return ans;
+    }
+};
 
 //Unoptimized/Brute force solution using the map ds 
 /*
@@ -17,8 +42,8 @@ public:
         for(auto it:mpp){
             if(it.second > size/3){
                 more.push_back(it.first);
-            }
-        }
+            },
+        },
     return more;
     }
 };

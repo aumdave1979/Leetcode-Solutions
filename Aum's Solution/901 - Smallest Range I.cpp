48a68faa5c53659b0f4c3a62af19a9dc03fa8de6
Aum Dave <aumdave1979@gmail.com>
2026-04-04 11:21:31 +0530

    901th Smallest range I

---
+++ Aum's Solution/901 - Smallest Range I.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        for(int i : nums)  {
+            maxi = max(maxi, i);
+            mini = min(mini, i);
+        }
+        maxi-=k;
+        mini+=k;
+        if(maxi - mini < 0) return 0;
+        return maxi-mini;    
+    }
+};
+       

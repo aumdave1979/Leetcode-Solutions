68da29cdf675bc9d7de0747817f0cdaf59e4ff49
aumdave1979 <aumdave1979@gmail.com>
2026-05-19 12:54:20 +0530

    binary search reccursive method

--- Aum's Striver Question/Binary_search/BS on 1D Arrays/Search X in Sorted Array.cpp
+++ Aum's Striver Question/Binary_search/BS on 1D Arrays/Search X in Sorted Array.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Reccursive Approch
+/*
+class Solution {
+public:
+    int bs(vector<int> &nums, int low, int high, int target){
+        if(low > high) return -1;
+        int mid = low + (high - low) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            return bs(nums, mid + 1, high, target);
+        }
+        return bs(nums, low, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        return bs(nums, 0, n - 1, target);
+    }
+};
+*/
+
+
 // Just a normal Typical Binary Search
 
 class Solution {

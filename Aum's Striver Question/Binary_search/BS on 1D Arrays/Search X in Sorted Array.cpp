b064d222e09372f34bfe0ebaacf57418674bb5ce
aumdave1979 <aumdave1979@gmail.com>
2026-05-19 12:06:50 +0530

    Binary Search 1d 1st question

---
+++ Aum's Striver Question/Binary_search/BS on 1D Arrays/Search X in Sorted Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Just a normal Typical Binary Search
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int n = nums.size();
+        int high = n-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(target == nums[mid]) return mid;
+            else if(target>nums[mid]){
+                low = mid+1;
+            }
+            else if(target<nums[mid]){
+                high = mid -1;
+            }
+        }
+    return -1;
+    }
+};

915333d852d639822c8936b4157752e2362b299b
aumdave1979 <aumdave1979@gmail.com>
2026-05-29 18:15:05 +0530

    367th

---
+++ Aum's Solution/367 - Valid Perfect Square.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 1;
+        int high = num;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            long long square = 1LL * mid * mid;
+
+            if(square == num){
+                return true;
+            }
+            if(square > num){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+};

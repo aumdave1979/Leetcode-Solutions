69eb3e76b8d7f28341ef7d830bdbe52de29a6e24
aumdave1979 <aumdave1979@gmail.com>
2026-06-05 10:56:04 +0530

    7th reverse Integer

---
+++ Aum's Solution/7  - Reverse Integer.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int revNum = 0;
+        while(x != 0){
+            int digit = x % 10;
+            x /= 10;
+            if(revNum > INT_MAX / 10 || revNum < INT_MIN / 10){
+                return 0;
+            }
+            revNum = revNum * 10 + digit;
+        }
+        return revNum;
+    }
+};

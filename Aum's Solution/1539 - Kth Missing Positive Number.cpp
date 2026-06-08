d439ac4c98a9b06370f6ad6e6aebec49bb4cf040
aumdave1979 <aumdave1979@gmail.com>
2026-06-08 14:51:56 +0530

    1539th kth missing positive number

---
+++ Aum's Solution/1539 - Kth Missing Positive Number.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// BS on Answers
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0;
+        int high = arr.size()-1;
+
+        while(low<=high){
+            int mid = low + ((high-low)/2);
+
+            int missing = arr[mid]- (mid+1);
+            if(missing < k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }      
+    return k+high+1;
+    }
+};
+
+
+/*  Brute force approch 
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=k){
+                k++;
+            }
+            else{
+                break;
+            }
+        }
+        return k;
+    }
+};
+*/

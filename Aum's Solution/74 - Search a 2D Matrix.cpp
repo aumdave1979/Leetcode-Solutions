1b886d7434c68ab9c0f1440255513acd21c63b7e
aumdave1979 <aumdave1979@gmail.com>
2026-06-24 16:32:21 +0530

    74th Search in 2d Matrix

--- Aum's Solution/74 - Search a 2D Matrix.cpp
+++ Aum's Solution/74 - Search a 2D Matrix.cpp
@@ -22,6 +22,26 @@ public:
 
 class Solution {
 public:
+
+    bool bs(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return true;
+
+            if (arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return false;
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
         int rows = matrix.size();
@@ -29,7 +49,7 @@ public:
 
         int topRow = 0;
         int bottomRow = rows - 1;
-        
+
         while (topRow <= bottomRow) {
 
             int midRow = topRow + (bottomRow - topRow) / 2;
@@ -41,23 +61,10 @@ public:
                 topRow = midRow + 1;
             }
             else {
-
-                int leftCol = 0;
-                int rightCol = cols - 1;
-                while (leftCol <= rightCol) {
-
-                    int midCol = leftCol + (rightCol - leftCol) / 2;
-                    if (matrix[midRow][midCol] == target)
-                        return true;
-
-                    if (matrix[midRow][midCol] < target)
-                        leftCol = midCol + 1;
-                    else
-                        rightCol = midCol - 1;
-                }
-                return false;
+                return bs(matrix[midRow], target);
             }
         }
+
         return false;
     }
 };

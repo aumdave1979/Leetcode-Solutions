5cd7dffeef44d41bbcf4c9f9d15ce351e1b3864d
aumdave1979 <aumdave1979@gmail.com>
2026-06-29 14:59:25 +0530

    preorder using stack

--- Aum's Solution/144 - Binary Tree Preorder Traversal.cpp
+++ Aum's Solution/144 - Binary Tree Preorder Traversal.cpp
@@ -1,6 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
  
+/* Using Stack less Space complexity
+ 
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if(root == NULL) return preorder;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root  = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right != NULL){
+                st.push(root->right);
+            }
+            if(root->left != NULL){
+                st.push(root->left);
+            }
+        }
+        return preorder;
+    }
+};
+*/
+
   struct TreeNode {
       int val;
       TreeNode *left;

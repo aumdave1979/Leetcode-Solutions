5c93a0547000dbfcefe64afa0470bd4e5688587d
aumdave1979 <aumdave1979@gmail.com>
2026-07-18 14:37:52 +0530

    same tree recurssive approch

--- Aum's Solution/100 - Same Tree.cpp
+++ Aum's Solution/100 - Same Tree.cpp
@@ -1,16 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-  //Definition for a binary tree node.
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
+//  Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==nullptr && q==nullptr){
+            return true;
+        }
+        if(p==nullptr && q!=nullptr) return false;
+        if(p!=nullptr && q==nullptr) return false;
+        if(p->val != q->val) return false;
+
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+
+ // Vector Based approch Traversal based solution
+/*
 class Solution {
 public:
     vector<int> data1;
@@ -43,4 +59,4 @@ public:
 
         return false;
     }
-};
+};*/

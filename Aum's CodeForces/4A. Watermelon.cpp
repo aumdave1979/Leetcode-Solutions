3ee10055775733a833fd24d8f5118c4696e902b1
aumdave1979 <aumdave1979@gmail.com>
2026-07-20 14:04:03 +0530

    started to understand and solve CodeForces 4A - Watermelon

---
+++ Aum's CodeForces/4A. Watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+
+    if(input>2 && input%2==0){
+        
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    return 0;
+}

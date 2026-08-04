0603a924cbefa317e6eec97e88fa470539227bba
aumdave1979 <aumdave1979@gmail.com>
2026-08-04 10:04:07 +0530

    zigzag Conversion 6th question

---
+++ Aum's Solution/6 - ZigZag Conversion.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1 || numRows >= s.size()){
+            return s;
+        }
+
+        vector<string> seq(numRows);
+        int currRow = 0;
+        bool incr = true;
+
+        for(int i=0;i<s.size();i++){
+            seq[currRow] += s[i];
+            if(currRow == 0){
+                incr = true;
+            }
+            else if(currRow == numRows-1){
+                incr = false;
+            }
+            if(incr){
+                currRow++;
+            }
+            else{
+                currRow--;
+            }
+        }
+
+        string ans;
+        for(int i=0;i<seq.size();i++){
+            ans+= seq[i];
+        }
+    return ans;
+    }
+};

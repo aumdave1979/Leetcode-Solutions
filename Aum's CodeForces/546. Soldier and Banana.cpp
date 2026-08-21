2d39e5b3c8d958f57fb20b778999bcd55f3273cc
aumdave1979 <aumdave1979@gmail.com>
2026-08-21 18:29:56 +0530

    2625 Sum Multiples

---
+++ Aum's CodeForces/546. Soldier and Banana.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int k; // for prices
+    int w; // no. of banana;
+    int n; // he has n dollars;
+    
+    cin >> k >> w >> n;
+    int total_sum = 0;
+}
